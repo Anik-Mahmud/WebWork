727a4d861baa2db287537c115c44ae50264c49ce
Anik-Mahmud <44143332+Anik-Mahmud@users.noreply.github.com>
2020-07-28 15:10:51 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+ 
+ <a href="https://app.powerbi.com/view?r=eyJrIjoiNmU2YWEwNTEtZWIwMC00M2Q2LTg5NzItMDI1YjEwNzM5NTNmIiwidCI6ImNkNTc1NTI0LTkyNTgtNGVkOC05NDg3LWUxYTIyN2JlMjkyYiIsImMiOjEwfQ%3D%3D" >বিস্তারিত</a>
+ 
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://corona.egeneration.co/">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
+
+<table class="table table-sm table-dark">
+ 
+  <tbody>
+
+<!-- On cells (`td` or `th`) -->
+<tr>
+  <td class="bg-primary"><h4>মৃত্যু</h4><h3>99</h3></td>
+  <td class="bg-success"><h4>আক্রান্ত</h4><h3>99</h3></td>
+  <td class="bg-warning"><h4>পরীক্ষা</h4><h3>99</h3></td>
+  <td class="bg-danger"><h4>সুস্থ</h4><h3>99</h3></td>
+</tr>
+  </tbody>
+</table>
+
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image/stay_home.jpg" alt="Stay Home" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Stay Home</h3>
+      <!--<p>We had such a great time in LA!</p>-->
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="image/11.jpg" alt="Corona" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Covid-19</h3>
+        <!--<p>Thank you, Chicago!</p>-->
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="image/12.jpg" alt="Patient" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>patien</h3>
+       <!-- <p>We love the Big Apple!</p>-->
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
